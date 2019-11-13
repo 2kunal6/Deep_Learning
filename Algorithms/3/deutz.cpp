e9288f8acbdce70244e1454e81113c02ef468b7c
2kunal6 <2kunal6@gmail.com>
2019-11-13 21:25:14 +0100

    ff deutz

--- Algorithms/3/deutz.cpp
+++ Algorithms/3/deutz.cpp
@@ -4,43 +4,53 @@
 
 using namespace std;
 
-bool g[10000][10000];
+bool g[20000][20000];
 
-bool allvis(int n) {
+int numcc(int n) {
+	int ans=0;
 	vector<bool> vis(n,0);
-	stack<int> s;
-	s.push(0);
-	while(!s.empty()) {
-		int val=s.top();
-		cout<<val<<endl;
-		s.pop();
-		vis[val]=1;
-		for(int i=0;i<n;i++) {
-			if(g[val][i]==1 && vis[i]==0) {
-				vis[i]=1;
-				s.push(i);
+	for(int i=0;i<n;i++) {
+		if(vis[i]==1)continue;
+		//if(g[3][4]==0)cout<<i<<endl;
+		stack<int> s;
+		s.push(i);
+		while(!s.empty()) {
+			int val=s.top();
+			s.pop();
+			vis[val]=1;
+			for(int j=0;j<n;j++) {
+				if(g[val][j]==1 && vis[j]==0) {
+					vis[j]=1;
+					s.push(j);
+				}
 			}
 		}
+		ans++;
 	}
-	for(int i=0;i<n;i++)if(vis[i]==0)return 0;
-	return 1;
+	return ans;
 }
 
 int main() {
-	int n,m,ans=0;
+	int n,m,ans=0,icc;
 	cin>>n>>m;
 	vector<pair<int, int> > v(m);
 	for(int i=0;i<m;i++) {
 		int x,y;
 		cin>>x>>y;
 		g[x][y]=1;
+		g[y][x]=1;
 		v[i].first=x;
 		v[i].second=y;
 	}
+	icc=numcc(n);
+	//cout<<icc<<endl;
 	for(int i=0;i<m;i++) {
 		g[v[i].first][v[i].second]=0;
-		if(!allvis(n))ans++;
+		g[v[i].second][v[i].first]=0;
+		if(numcc(n)>icc)ans++;
+		//cout<<numcc(n)<<endl;
 		g[v[i].first][v[i].second]=1;
+		g[v[i].second][v[i].first]=1;
 	}
 	cout<<ans<<endl;
 	return 0;
--- Algorithms/3/ff.cpp
+++ Algorithms/3/ff.cpp
@@ -17,11 +17,11 @@ long long cal() {
 		ans+=(abs(px-it->first));
 		px=it->first;
 		if(abs(it->second.first-py)>abs(it->second.second-py)) {
-			py=it->second.first;
-			ans+=(abs(it->second.second-py));
-		} else {
 			py=it->second.second;
 			ans+=(abs(it->second.first-py));
+		} else {
+			py=it->second.first;
+			ans+=(abs(it->second.second-py));
 		}
 		//cout<<ans<<endl;
 	}
@@ -29,7 +29,7 @@ long long cal() {
 	ans+=(abs(px-sx)+abs(py-sy));
 	return ans;
 }
-long long caly() {
+/*long long caly() {
 	long long ans=0;
 	px=sx;py=sy;
 	for(it=my.begin();it!=my.end();it++) {
@@ -43,12 +43,12 @@ long long caly() {
 			py=it->second.second;
 			ans+=(abs(it->second.first-py));
 		}
-		//cout<<ans<<endl;
+		cout<<ans<<endl;
 	}
 	//cout<<px<<" "<<py<<" "<<sx<<" "<<sy<<endl;
 	ans+=(abs(px-sx)+abs(py-sy));
 	return ans;
-}
+}*/
 int main() {
 	long long n,x,y,ans1=0,ans2=0;
 	cin>>n;
@@ -64,19 +64,19 @@ int main() {
 			if(y<it->second.first)it->second.first=y;
 			if(y>it->second.second)it->second.second=y;
 		}
-		it=my.find(y);
+		/*it=my.find(y);
 		if(it==my.end())my[y]=make_pair(x,x);
 		else {
 			if(x<it->second.first)it->second.first=x;
 			if(x>it->second.second)it->second.second=x;
-		}
+		}*/
 	}
 	ans1=cal();
+	cout<<ans1<<endl;
 
-	swap(sx, sy);
+	/*swap(sx, sy);
 	ans2=caly();
-
-	cout<<max(ans1,ans2)<<endl;
+	cout<<max(ans1,ans2)<<endl;*/
 	
 	return 0;
 }
