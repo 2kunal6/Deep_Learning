78df0bdb95b7461708af5db6a1c884369cc37996
2kunal6 <2kunal6@gmail.com>
2019-11-16 12:44:48 +0100

    furious

--- Algorithms/3/ff.cpp
+++ Algorithms/3/ff.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-	long long sx,sy,x,y,n,ans=0,dist=1000000000000ll,diff,rv;
+	long long px,py,sx,sy,x,y,n,ans=0,dist=1000000000000ll,diff,rv;
 	cin>>n;
 	cin>>sx>>sy;
 	vector<pair<long long,long long> > v;
@@ -15,22 +15,23 @@ int main() {
 		cin>>x>>y;
 		v.push_back(make_pair(x,y));
 	}
-
+	px=sx;py=sy;
 	for(int i=0;i<n;i++) {
 		for(int j=0;j<n;j++) {
 			if(v[j].first==1000000000000ll)continue;
-			diff=abs(sx-v[j].first)+abs(sy-v[j].second);
+			diff=abs(px-v[j].first)+abs(py-v[j].second);
 			if(diff<dist) {
 				dist=diff;
-				sx=v[j].first;
-				sy=v[j].second;
+				px=v[j].first;
+				py=v[j].second;
 				rv=j;
 			}
 		}
 		v[rv].first=v[rv].second=1000000000000ll;
 		dist=1000000000000ll;
-		ans+=dist;
+		ans+=diff;
 	}
+	ans+=abs(px-sx)+abs(py-sy);
 
 	cout<<ans<<endl;
 
