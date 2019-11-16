65ce8d95d8cff26ddf1448ddea9ffdc0a55737d6
2kunal6 <2kunal6@gmail.com>
2019-11-16 17:16:54 +0100

    fast and furios MST preorder walk

--- Algorithms/3/ff.cpp
+++ Algorithms/3/ff.cpp
@@ -1,39 +1,74 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<stack>
 
 using namespace std;
 
-int main() {
-	long long px,py,sx,sy,x,y,n,ans=0,dist=1000000000000ll,diff,rv;
-	cin>>n;
-	cin>>sx>>sy;
-	vector<pair<long long,long long> > v;
+struct edge {
+	long long len;
+	int from, to;
+};
+long long x,y,n,ans=0;
+vector<pair<long long, long long> > nodes;
+vector<edge> edges;
+vector<vector<int> > mst;
 
-	v.push_back(make_pair(sx,sy));
-	for(int i=1;i<n;i++) {
-		cin>>x>>y;
-		v.push_back(make_pair(x,y));
-	}
-	px=sx;py=sy;
+bool mycom(edge a, edge b) {
+	return a.len<b.len;
+}
+void kruskal() {
+	vector<bool> vis(n,0);
 	for(int i=0;i<n;i++) {
-		for(int j=0;j<n;j++) {
-			if(v[j].first==1000000000000ll)continue;
-			diff=abs(px-v[j].first)+abs(py-v[j].second);
-			if(diff<dist) {
-				dist=diff;
-				px=v[j].first;
-				py=v[j].second;
-				rv=j;
-			}
-		}
-		v[rv].first=v[rv].second=1000000000000ll;
-		dist=1000000000000ll;
-		ans+=diff;
+		vector<int> temp;
+		mst.push_back(temp);
 	}
-	ans+=abs(px-sx)+abs(py-sy);
-
+	for(int i=0;i<edges.size();i++) {
+		if(vis[edges[i].from]==1 && vis[edges[i].to]==1)continue;
+		vis[edges[i].from]=vis[edges[i].to]=1;
+		mst[edges[i].from].push_back(edges[i].to);
+	}
+}
+void preord() {
+	stack<int> s;
+	s.push(0);
+	int prev=0;
+	while(!s.empty()) {
+		int val=s.top();
+		//cout<<val<<endl;
+		s.pop();
+		ans+=(abs(nodes[prev].first-nodes[val].first)+abs(nodes[prev].second+nodes[val].second));
+		prev=val;
+		for(int j=0;j<mst[val].size();j++) {
+			s.push(mst[val][j]);
+		}
+	}
+	ans+=(abs(nodes[prev].first-nodes[0].first)+abs(nodes[prev].second+nodes[0].second));
+}
+int main() {
+	cin>>n;
+	for(int i=0;i<n;i++) {
+		cin>>x>>y;
+		nodes.push_back(make_pair(x,y));
+		for(int j=0;j<i;j++) {
+			edge e;
+			e.from=j;
+			e.to=i;
+			e.len=abs(nodes[e.from].first-nodes[e.to].first)+abs(nodes[e.from].second-nodes[e.to].second);
+			edges.push_back(e);
+			swap(e.from,e.to);
+			edges.push_back(e);
+		}
+	}
+	sort(edges.begin(), edges.end(), mycom);
+	//for(int i=0;i<edges.size();i++)cout<<edges[i].len<<" "<<edges[i].from<<" "<<edges[i].to<<endl;
+	kruskal();
+	/*for(int i=0;i<mst.size();i++) {
+		cout<<i<<":";
+		for(int j=0;j<mst[i].size();j++)cout<<mst[i][j]<<" ";
+		cout<<endl;
+	}*/
+	preord();
 	cout<<ans<<endl;
-
 	return 0;
 }
