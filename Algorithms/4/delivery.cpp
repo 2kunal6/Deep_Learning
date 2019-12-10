00137e988d545ab7588e8f2ab6037ccd9e8a6eb5
2kunal6 <2kunal6@gmail.com>
2019-12-10 15:20:44 +0100

    delivery

--- Algorithms/4/delivery.cpp
+++ Algorithms/4/delivery.cpp
@@ -8,8 +8,9 @@ using namespace std;
 
 vector<vector<double> > g;
 int src,dst,n;
+vector<pair<int, int> > cs;
 
-double dijk() {
+void dijk() {
 	double dist[n];
 	bool sp[n];
 	for(int i=0;i<n;i++) {
@@ -27,43 +28,47 @@ double dijk() {
 			}
 		}
 		sp[minv]=1;
-		if(minv==dst)return dist[dst];
 		for(int j=0;j<n;j++) {
 			if(!sp[j] && g[minv][j]!=-1 && dist[minv]!=DBL_MAX && (dist[minv]+g[minv][j]<dist[j]))dist[j]=dist[minv]+g[minv][j];
 		}
-		//for(int j=0;j<n;j++)cout<<dist[j]<<" ";
-		//cout<<endl;
 	}
-	return dist[dst];
+	for(int i=0;i<n;i++) {
+		if(dist[i]>cs[i].first)g[src][i]=DBL_MAX;
+		else g[src][i]/=(double)cs[i].second;
+	}
 }
 
 int main() {
-	int q;
+	int q, csf, css;
 	cin>>n>>q;
 
-	vector<pair<int, int> > cs(n);
-	for(int i=0;i<n;i++)cin>>cs[i].first>>cs[i].second;
+	for(int i=0;i<n;i++) {
+		cin>>csf>>css;
+		cs.push_back(make_pair(csf,css));
+	}
 
 	for(int i=0;i<n;i++) {
 		vector<double> tmp(n);
 		for(int j=0;j<n;j++){
 			cin>>tmp[j];
-			if(cs[i].first<tmp[j])tmp[j]=-1;
-			if(tmp[j]==-1)continue;
-			tmp[j]/=(double)cs[i].second;
+			if(tmp[j]==-1)tmp[j]=DBL_MAX;
 		}
 		g.push_back(tmp);
+	}
+	for(int i=0;i<n;i++) {
+		src=i;
+		dijk();
 	}
 	for(int i=0;i<n;i++) {
 		for(int j=0;j<n;j++)cout<<g[i][j]<<" ";
 		cout<<endl;
 	}
-	while(q--) {
+	/*while(q--) {
 		cin>>src>>dst;
 		src--;dst--;
 		cout << fixed << setprecision(2);
 		cout<<dijk()<<" ";
-	}
+	}*/
 	cout<<endl;
 	return 0;
 }
