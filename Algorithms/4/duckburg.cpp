81ccd949a9a28b936b648fca39dfdb66ea029ff1
2kunal6 <2kunal6@gmail.com>
2019-12-11 21:24:36 +0100

    duckburg

---
+++ Algorithms/4/duckburg.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+
+using namespace std;
+
+long long dp[1001][101];
+int main() {
+	int k,n;
+	cin>>k>>n;
+	vector<int> v(n);
+	for(int i=0;i<n;i++)cin>>v[i];
+	sort(v.begin(), v.end());
+
+	for(int i=1;i<=n;i++) {
+		for(int m=1;m<=k;m++) {
+			d[i][m]=min()
+		}
+	}
+}
--- Algorithms/4/kamehameha.cpp
+++ Algorithms/4/kamehameha.cpp
@@ -1,31 +1,23 @@
 #include<iostream>
-#include<map>
 #include<algorithm>
-#include<string>
 #include<vector>
 
 using namespace std;
 
-vector<long long> v;
-map<string, long long> mp;
-
-long long part(int n, long long s1, long long s2) {
-	if(n<0)return max(s1,s2);
-	string key=to_string(n)+">"+to_string(s1);
-	if(mp.find(key)==mp.end()) {
-		long long inc=part(n-1, s1+v[n], s2);
-		long long exc=part(n-1, s1, s2+v[n]);
-		mp[key]=min(inc, exc);
-	}
-	return mp[key];
-}
 int main() {
-	int n,x;
+	int n;
+	long long s1=0,s2=0;
 	cin>>n;
+	vector<long long> v(n);
+	for(int i=0;i<n;i++)cin>>v[i];
+	
+	sort(v.begin(), v.end());
+	reverse(v.begin(), v.end());
+
 	for(int i=0;i<n;i++) {
-		cin>>x;
-		v.push_back((long long)x);
+		if(s1<s2)s1+=v[i];
+		else s2+=v[i];
 	}
-	cout<<part(n-1, 0, 0)<<endl;
+	cout<<max(s1,s2)<<endl;
 	return 0;
 }
