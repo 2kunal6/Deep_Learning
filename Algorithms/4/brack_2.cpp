4d4bd4ff043d2e59bae4ee7864dbadbfab63aa76
2kunal6 <2kunal6@gmail.com>
2019-12-13 18:53:11 +0100

    brack

--- Algorithms/4/brack_2.cpp
+++ Algorithms/4/brack_2.cpp
@@ -6,7 +6,6 @@
 using namespace std;
 
 int vlen;
-stack<bitset<2> > s;
 vector<bitset<2> > v;
 
 bool bal(stack<bitset<2> > ls, int ind) {
@@ -15,25 +14,26 @@ bool bal(stack<bitset<2> > ls, int ind) {
 	bool res;
 	if(v[ind][0]==1) {
 		ls.push(v[ind]);
-		return bal(s, ind+1);
+		return bal(ls, ind+1);
 	} else if(v[ind][1]==1) {
 		if(ls.empty())return 0;
 		top=ls.top();
 		ls.pop();
-		return bal(s, ind+1);
+		return bal(ls, ind+1);
 	} else {
 		stack<bitset<2> > tmp=ls;
 		tmp.push(v[ind]);
 		res=bal(tmp, ind+1);
 		if(res)return 1;
-		if(s.empty())return 0;
-		s.pop();
-		return bal(s, ind+1);
+		if(ls.empty())return 0;
+		ls.pop();
+		return bal(ls, ind+1);
 	}
 }
 
 int main() {
 	char c;
+	stack<bitset<2> > s;
 	while(std::cin>>c) {
 		if(c=='(')v.push_back(bitset<2>(1));
 		if(c==')')v.push_back(bitset<2>(2));
