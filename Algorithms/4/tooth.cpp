b2e30a1ebe987eeb71f5a884498a1ccf58b3c9f9
Kunal Rout <Kunal.Rout@iais.fraunhofer.de>
2019-12-13 11:45:18 +0100

    tooth

--- Algorithms/4/tooth.cpp
+++ Algorithms/4/tooth.cpp
@@ -3,21 +3,34 @@
 
 using namespace std;
 
-bool satisfy() {
-	for(int i=0;i<)
+int c,t,ans=-1,tmp;
+vector<vector<int> > cp;
+vector<int> ct;
+
+bool satisfy(int p, int cn) {
+	bool sat=0;
+	for(int i=0;i<c;i++) {
+		for(int j=0;j<=p;j++) {
+			if(cp[i][j]==0)continue;
+			cp[i][j]--;
+			sat=satisfy(p, i+1);
+			cp[i][j]++;
+		}
+	}
+	return sat;
 }
 int main() {
-	int c,t,ans=-1;
-	vector<int> ct(t);
-	vector<vector<int> > cp;
-	for(int i=0;i<t;i++)cin>>ct[i];
+	for(int i=0;i<t;i++) {
+		cin>>tmp;
+		ct.push_back(tmp);
+	}
 	for(int i=0;i<c;i++) {
 		vector<int> prf(t);
 		for(int j=0;j<t;j++)cin>>prf[j];
 		cp.push_back(prf);
 	}
 	for(int i=0;i<t;i++) {
-		if(satisfy()) {
+		if(satisfy(i, 0)) {
 			ans=i;
 			break;
 		}
