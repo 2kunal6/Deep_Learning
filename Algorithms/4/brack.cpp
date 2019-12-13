a529977e48816f46ac927ea725030cdaf6c52e86
Kunal Rout <Kunal.Rout@iais.fraunhofer.de>
2019-12-13 12:57:12 +0100

    brack

--- Algorithms/4/brack.cpp
+++ Algorithms/4/brack.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-	stack<bit> s;
+	stack<bool> s;
 	char c;
 	while(std::cin>>c) {
 		if(c=='(')s.push(1);
@@ -17,7 +17,17 @@ int main() {
 			s.pop();
 		}
 	}
-	int 
-	while(!)
+	int cnt=0;	
+	while(!s.empty()) {
+		if(s.top()==0)cnt++;
+		else cnt--;
+		if(cnt<0) {
+			cout<<0<<endl;
+			return 0;
+		}
+		s.pop();
+	}
+	if(cnt%2==0)cout<<1<<endl;
+	else cout<<0<<endl;
 	return 0;
 }
