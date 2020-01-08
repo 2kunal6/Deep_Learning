767fef31d170c0cfecd780c699230b6f3d834684
2kunal6 <2kunal6@gmail.com>
2020-01-08 20:35:29 +0100

    skyline

--- Algorithms/5/skyline.cpp
+++ Algorithms/5/skyline.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
 #include<vector>
-#include<queue>
 
 using namespace std;
 
 int main() {
-	int n,ans;
+	long long n,ans;
 	cin>>n;
 	ans=n*n+2;
 	vector<vector<long long> > g;
@@ -14,17 +13,26 @@ int main() {
 		for(int j=0;j<n;j++)cin>>t[j];
 		g.push_back(t);
 	}
-	queue<pair<int, int> > q;
-	int hov[n*n][2];
-	for(int i=0;i<n*n;i++)hov[i][0]=hov[i][1]=-1;
+	vector<vector<pair<long long, long long> > > hov;
+	for(int i=0;i<(n*n)+2;i++) {
+		vector<pair<long long, long long> > tv;
+		hov.push_back(tv);
+	}
+
 	for(int i=0;i<n;i++) {
 		for(int j=0;j<n;j++) {
-			if(i+1<n)q.push(make_pair(i+1, j));
-			if(j+1<n)q.push(make_pair(i, j+1));
-			if(hov[g[i][j]][0]!=-1) {
-				int px=hov[g[i][j]][0], py=hov[g[i][j]][1];
-				if(ans<)
+			hov[g[i][j]].push_back(make_pair(i, j));
+		}
+	}
+	for(int i=0;i<(n*n)+2;i++) {
+		if(hov[i].size()<2)continue;
+		for(int j=0;j<hov[i].size();j++) {
+			for(int k=j+1;k<hov[i].size();k++) {
+				long long tans=abs(hov[i][j].first-hov[i][k].first)+abs(hov[i][j].second-hov[i][k].second);
+				if(tans<ans)ans=tans;
 			}
 		}
 	}
+	cout<<ans<<endl;
+	return 0;
 }
